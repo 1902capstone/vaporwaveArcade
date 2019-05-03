d4cf9ce767e3cd4d24db7fd15b3dee796bee06fc
Josh Kirschenbaum <JoshKBaum@gmail.com>
2019-05-03 16:02:25 -0500

    tasting animation

--- js/JoshScene.js
+++ js/JoshScene.js
@@ -19,13 +19,12 @@ import {
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
-
   constructor() {
     super();
 
     // Set initial state here
     this.state = {
-      text: "Initializing AR..."
+      text: 'Initializing AR...',
     };
 
     // bind 'this' to functions
@@ -34,36 +33,59 @@ export default class HelloWorldSceneAR extends Component {
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-        
-        <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} animation={{name: "rotate", run: true, loop: true}}/>
-        
-        <ViroAmbientLight color={"#aaaaaa"} />
-        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0, -1, -.2]}
-          position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
-          
-        <ViroNode position={[0, -1, 0]} dragType="FixedToWorld" onDrag={() => { }} >
-          <Viro3DObject
-            animation={{name: "rotate", run: true, loop: true}}
-            source={require('./res/emoji_smile/emoji_smile.vrx')}
-            resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
-            require('./res/emoji_smile/emoji_smile_normal.png'),
-            require('./res/emoji_smile/emoji_smile_specular.png')]}
-            position={[-.5, .5, -1]}
-            scale={[.2, .2, .2]}
-            type="VRX" />
-        </ViroNode>
-        
-      </ViroARScene>
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
 
+        <ViroBox
+          position={[0, -0.5, -1]}
+          scale={[0.3, 0.3, 0.1]}
+          materials={['grid']}
+          animation={{ name: 'rotate', run: true, loop: true }}
+        />
+
+        <ViroAmbientLight color={'#aaaaaa'} />
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={90}
+          direction={[0, -1, -0.2]}
+          position={[0, 3, 1]}
+          color="#ffffff"
+          castsShadow={true}
+        />
+
+        <ViroNode
+          position={[0, -1, 0]}
+          dragType="FixedToWorld"
+          onDrag={() => {}}
+        >
+          <Viro3DObject
+            animation={{ name: 'rotate', run: true, loop: true }}
+            source={require('./res/emoji_smile/emoji_smile.vrx')}
+            opacity={0.5}
+
+            resources={[
+              require('./res/emoji_smile/emoji_smile_diffuse.png'),
+              require('./res/emoji_smile/emoji_smile_normal.png'),
+              require('./res/emoji_smile/emoji_smile_specular.png'),
+            ]}
+            position={[-0.5, 0.5, -1]}
+            scale={[0.2, 0.2, 0.2]}
+            type="VRX"
+          />
+        </ViroNode>
+      </ViroARScene>
     );
   }
 
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text: "Worlds SICKEST box!"
+        text: 'Worlds SICKEST box!',
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
@@ -83,18 +105,17 @@ var styles = StyleSheet.create({
 
 ViroMaterials.createMaterials({
   grid: {
-    diffuseTexture: require('./res/grid_bg.jpg')
-  }
-})
+    diffuseTexture: require('./res/grid_bg.jpg'),
+  },
+});
 
 ViroAnimations.registerAnimations({
   rotate: {
     properties: {
-      rotateY: "+=90"
+      rotateY: '+=30',
     },
-    duration: 250, //.25 seconds
+    duration: 2500, //.25 seconds
   },
 });
-
 
 module.exports = HelloWorldSceneAR;
