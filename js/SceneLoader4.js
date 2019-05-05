a47add6f6218dd9d2026b89d3b7a657016595320
Armon Davani <aaarrm@gmail.com>
2019-05-05 18:04:47 -0500

    menu working

---
+++ js/SceneLoader4.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  View,
+  StyleSheet,
+  PixelRatio,
+  Image,
+  ImageBackground,
+  TouchableHighlight,
+} from 'react-native';
+
+import {
+  ViroARSceneNavigator
+} from 'react-viro';
+
+
+const API_KEY = "4B132E39-801E-47A0-8F11-E44215B1CE84";
+
+const ARHelloWorld = require('./HelloWorldSceneAR');
+const Game4 = require('./Game4')
+
+
+export default class SceneLoader4 extends Component {
+  constructor() {
+    super()
+  }
+  
+  render() {
+    return (
+      <ViroARSceneNavigator apiKey={API_KEY} initialScene = {{ scene: ARHelloWorld}} />
+    )
+  }
+  
+}
+
+module.exports = SceneLoader4
