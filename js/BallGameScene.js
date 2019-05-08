3bb7087b81052f0291fae55c76975c43208e3c6e
Armon Davani <aaarrm@gmail.com>
2019-05-08 14:45:33 -0500

    completed ball game infrastructure

--- js/BallGameScene.js
+++ js/BallGameScene.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 import React, { Component } from 'react';
 
@@ -27,6 +27,15 @@ import {
   ViroNode,
 } from 'react-viro';
 
+
+let spheres = [];
+let timerStarted = false;
+let timerIntervalId;
+let ballSpawnIntervalId;
+let gameStarted = false;
+let sphereCount = 0;
+
+
 export default class JoshScene extends Component {
   constructor() {
     super();
@@ -34,174 +43,48 @@ export default class JoshScene extends Component {
     // Set initial state here
     this.state = {
       text: 'Initializing AR...',
+      spheres: 0,
+      startTime: 0,
     };
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
-    this._onButtonTap = this._onButtonTap.bind(this);
+    this.createSpheres = this.createSpheres.bind(this);
+    this.renderSpheres = this.renderSpheres.bind(this);
+    // this.handleTime = this.handleTime.bind(this);
+    this.handleGameStart = this.handleGameStart.bind(this);
+    this.handleScore = this.handleScore.bind(this);
   }
 
-  render() {
-    const currentScore = this.props.arSceneNavigator.viroAppProps.score
-    return (
 
+  componentDidMount() {
+    gamestarted = false;
+    spheres = [];
+  }
+
+  
+  componentWillUnmount() {
+    clearInterval(ballSpawnIntervalId)
+    ballSpawnIntervalId = 0;
+    spheres = [];
+  }
+  
+  render() {
+    //const timer = this.props.arSceneNavigator.viroAppProps.timer
+
+    return (
       <ViroARScene
         onTrackingUpdated={this._onInitialized}
-        physicsWorld={{ gravity: [0, -3, 0] }}
+        physicsWorld={{ gravity: [0, -1.8, 0] }}
       >
-        <ViroARPlaneSelector
-          minHeight={0.01}
-          minWidth={0.01}
-          onPlaneSelected={() => {
-            this.setState({ pauseUpdates: true });
-          }}
-          pauseUpdates={this.state.pauseUpdates}
-        >
-          <ViroSphere
-            // viroTag="BallTag"
-            heightSegmentCount={20}
-            widthSegmentCount={20}
-            radius={0.5}
-            position={[0, 1.5, -4]}
-            height={1}
-            materials={['blue']}
-            physicsBody={{
-              type: 'Dynamic',
-              mass: 8,
-              restitution: 1,
-            }}
-          />
-          <ViroSphere
-            // viroTag="BallTag"
-            heightSegmentCount={20}
-            widthSegmentCount={20}
-            radius={0.5}
-            position={[0.5, 1, -4]}
-            height={1}
-            materials={['purple']}
-            physicsBody={{
-              type: 'Dynamic',
-              mass: 8,
-              restitution: 0.999,
-            }}
-          />
-          <ViroSphere
-            // viroTag="BallTag"
-            heightSegmentCount={20}
-            widthSegmentCount={20}
-            radius={0.5}
-            position={[0.5, 2, -2]}
-            height={1}
-            materials={['purple']}
-            physicsBody={{
-              type: 'Dynamic',
-              mass: 8,
-              restitution: 0.999,
-            }}
-          />
-          <ViroSphere
-            // viroTag="BallTag"
-            heightSegmentCount={20}
-            widthSegmentCount={20}
-            radius={0.5}
-            position={[0.5, 2, -3]}
-            height={1}
-            materials={['purple']}
-            physicsBody={{
-              type: 'Dynamic',
-              mass: 8,
-              restitution: 0.999,
-            }}
-          />
-          <ViroSphere
-            // viroTag="BallTag"
-            heightSegmentCount={20}
-            widthSegmentCount={20}
-            radius={0.5}
-            position={[0.5, 2, -1]}
-            height={1}
-            materials={['purple']}
-            physicsBody={{
-              type: 'Dynamic',
-              mass: 8,
-              restitution: 0.999,
-            }}
-          />
-          <ViroSphere
-            // viroTag="BallTag"
-            heightSegmentCount={20}
-            widthSegmentCount={20}
-            radius={0.5}
-            position={[0.5, 2, -1]}
-            height={1}
-            materials={['purple']}
-            physicsBody={{
-              type: 'Dynamic',
-              mass: 8,
-              restitution: 0.999,
-            }}
-          />
-            <ViroSphere
-            // viroTag="BallTag"
-            heightSegmentCount={20}
-            widthSegmentCount={20}
-            radius={0.5}
-            position={[0, -1, -1]}
-            height={1}
-            materials={['purple']}
-            physicsBody={{
-              type: 'Dynamic',
-              mass: 8,
-              restitution: 0.999,
-            }}
-          />
-          <ViroQuad
-            position={[0, -2, -4]}
-            height={7}
-            width={4}
-            rotation={[-87, 0, 0]}
-            opacity={0.8}
-            physicsBody={{ type: 'Static', restitution: 1 }}
-            materials={['red']}
-          />
-
-          {/* <ViroText 
-            text={this.props.arSceneNavigator.viroAppProps.score}
-            width={2}
-            height={2}
-            position={[0,0,-5]}
-            extrusionDepth={1}
-          /> */}
-
-          <ViroText
-            text={currentScore.toString()}
-            scale={[.5, .5, .5]}
-            position={[0, 0, -1]}
-            style={localStyles.helloWorldTextStyle} />
-
-          <ViroARCamera>
-            <Viro3DObject
-              animation={{ name: 'rotate', run: true, loop: true }}
-              source={require('./res/Skull.obj')}
-              opacity={0.2}
-              // resources={[
-              //   require('./res/emoji_smile/emoji_smile_diffuse.png'),
-              //   require('./res/emoji_smile/emoji_smile_normal.png'),
-              //   require('./res/emoji_smile/emoji_smile_specular.png'),
-              // ]}
-              position={[0, -.5, -1]}
-              scale={[0.0008, 0.0008, 0.0008]}
-              type="OBJ"
-              physicsBody={{type: 'Static'}}
-              onCollision={this.props.arSceneNavigator.viroAppProps.incrementScore}
-            />
-          </ViroARCamera>
-        </ViroARPlaneSelector>
+        {this.renderARScene()}
       </ViroARScene>
     );
   }
 
   _onInitialized(state, reason) {
+
+
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
         text: 'Welcome to the arcade!',
@@ -210,16 +93,207 @@ export default class JoshScene extends Component {
       // Handle loss of tracking
     }
   }
-  _onButtonGaze() {
+
+
+
+
+
+  createSpheres() {
+    const spheresToLoad = [];
+    const numOfSpheres = Math.floor(Math.random() * 2) + 3
+
+    for (let i = 0; i < numOfSpheres; i++) {
+      const sphereTag = `sphere-${sphereCount + 1}`
+      sphereCount++;
+      
+
+      const randomYPos = (Math.random() * 2) + 0.5
+      // between 0.5 and 2.5
+      const randomZPos = ((Math.random() * 3) + 1.5) * -1
+      // between -1.5 and -4.5
+      const randomXPos = ((Math.random() * 3)  - 1.5)
+      // between -1.5 and 1.5
+      const randomMass = Math.floor(Math.random() * 11) + 1
+      // between 1 and 12
+      const randomRad = Math.floor(Math.random() * 0.3) + 0.2
+      // between 0.2 and 0.5
+      
+      
+      const x = <ViroSphere
+        key={sphereTag}
+        viroTag={sphereTag}
+        heightSegmentCount={20}
+        widthSegmentCount={20}
+        radius={randomRad}
+        position={[randomXPos, randomYPos, -3]}
+        height={1}
+        materials={['purple']}
+        physicsBody={{
+          type: 'Dynamic',
+          mass: randomMass,
+          restitution: 0.999,
+        }}
+
+      />
+      const SphereObj = {
+        show: false,
+        model: x,
+        num: spheres.length + 1,
+        time: 0,
+      }
+      spheres.push(SphereObj);
+
+    }
+
+    // const sphereTag = `sphere-${sphereCount+1}`
+    // sphereCount++;
+    // const x = <ViroSphere
+    //   key = {sphereTag}
+    //   viroTag = {sphereTag}
+    //   heightSegmentCount={20}
+    //   widthSegmentCount={20}
+    //   radius={0.7}
+    //   position={[0.5, 1, -4]}
+    //   height={1}
+    //   materials={['purple']}
+    //   physicsBody={{
+    //     type: 'Dynamic',
+    //     mass: 8,
+    //     restitution: 0.999,
+    //   }}
+
+    // />
+    //   const SphereObj = {
+    //     show: false,
+    //     model: x,
+    //     num: spheres.length+1,
+    //     time: 0,
+    //   }
+    //   spheres.push(SphereObj);
+
     this.setState({
-      buttonStateTag: 'onGaze',
-    });
+      spheres: this.state.spheres + spheresToLoad.length
+    })
+
   }
-  _onButtonTap() {
+
+
+  renderSpheres() {
+    let sphereList = spheres.map((item) => {
+      return item.model;
+    })
+    return sphereList;
+  }
+
+  // handleTime() {
+  //   if (!timerStarted) {
+  //     console.log(this.props.arSceneNavigator.viroAppProps)
+  //     const setTimer = this.props.arSceneNavigator.viroAppProps.setTimer
+  //     const myStartTime = this.state.startTime
+  //     this.setState({
+  //       startTime: Date.now()
+  //     })
+  //     const findNewTime = function(setTimerFunc, myStartTimeVar) {
+  //       const currentTime = Date.now();
+  //       let timeElapsed = currentTime - myStartTimeVar;
+  //       setTimerFunc(timeElapsed);
+  //     }
+
+  //     timerIntervalId = setInterval(findNewTime(setTimer, myStartTime), 1000);
+  //     timerStarted = true;
+  //   }
+  // }
+
+  handleGameStart() {
+    if (!ballSpawnIntervalId && this.state.startTime) {
+      this.props.arSceneNavigator.viroAppProps.beginTimer()
+      ballSpawnIntervalId = setInterval(this.createSpheres, 1800)
+    }
+  }
+
+  handleScore(colliderTag) {
+
+
+    let indexOfSphere = spheres.findIndex((elt) => {
+      return (elt.model.props.viroTag === colliderTag)
+    })
+    spheres.splice(indexOfSphere, 1);
+
     this.setState({
-      buttonStateTag: 'onTap',
-    });
+      spheres: this.state.spheres - 1
+    })
+    this.props.arSceneNavigator.viroAppProps.incrementScore()
   }
+
+  renderARScene() {
+
+    const currentScore = this.props.arSceneNavigator.viroAppProps.score
+    const timer = this.props.arSceneNavigator.viroAppProps.timer
+
+    return (
+
+      <ViroARPlaneSelector
+        minHeight={0.01}
+        minWidth={0.01}
+        onPlaneSelected={() => {
+          this.handleGameStart()
+          this.setState({ pauseUpdates: true, startTime: Date.now() });
+        }}
+        pauseUpdates={this.state.pauseUpdates}
+      >
+
+
+
+        {/* {this.handleTime()} */}
+        {this.handleGameStart()}
+        {this.renderSpheres()}
+
+
+        <ViroQuad
+          position={[0, -2, -4]}
+          height={7}
+          width={4}
+          rotation={[-87, 0, 0]}
+          opacity={0.8}
+          physicsBody={{ type: 'Static', restitution: 1 }}
+          materials={['red']}
+        />
+
+        <ViroText
+          text={currentScore.toString()}
+          scale={[.5, .5, .5]}
+          position={[0, 0, -1]}
+          style={localStyles.helloWorldTextStyle} />
+
+
+        <ViroARCamera>
+          <Viro3DObject
+            animation={{ name: 'rotate', run: true, loop: true }}
+            source={require('./res/Skull.obj')}
+            opacity={0.2}
+            // resources={[
+            //   require('./res/emoji_smile/emoji_smile_diffuse.png'),
+            //   require('./res/emoji_smile/emoji_smile_normal.png'),
+            //   require('./res/emoji_smile/emoji_smile_specular.png'),
+            // ]}
+            position={[0, -.5, -1]}
+            scale={[0.0008, 0.0008, 0.0008]}
+            type="OBJ"
+            physicsBody={{ type: 'Static' }}
+            // onCollision={this.props.arSceneNavigator.viroAppProps.incrementScore}
+            onCollision={this.handleScore}
+            onClick={this.createSpheres}
+          />
+        </ViroARCamera>
+      </ViroARPlaneSelector>
+
+
+    )
+
+  }
+
+
+
 }
 
 var localStyles = StyleSheet.create({
